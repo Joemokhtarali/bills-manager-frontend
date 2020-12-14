1a3c2e1af9bea64fa08fccf7c4e1a2ce7ea82da6
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-12-14 18:47:55 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
+import About from './components/About'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+      <Route exact path="/"> <Home /> </Route>
+      <Route path="/about"> <About /> </Route>
+    </Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
