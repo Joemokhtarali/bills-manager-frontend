eeec10b91e12ec1bb1fb6e7da73efc4d8c521388
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-12-14 19:49:14 -0500

    form updated with user and password

--- src/forms/login.js
+++ src/forms/login.js
@@ -1,30 +1,50 @@
-import {React , useState} from 'react'
+import { React, useState } from "react";
 
-export default function Login(){
-    const [userName, setuserName] = useState('')
-    const [password, setPassword] = useState('')
+export default function Login() {
+  const [userName, setuserName] = useState("");
+  const [password, setPassword] = useState("");
 
-    const changeUserName = (e) => {
-        setuserName(e.target.value)
-    }
+  const changeUserName = (e) => {
+    setuserName(e.target.value);
+  };
 
+  const changepassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const changepassword = (e) => {
-        setPassword(e.target.value)
-    }
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    console.log('Login', userName, password);
+  };
+  const handleSignupSubmit = (e) => {
+    e.preventDefault();
+    console.log('Signup', userName, password);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault() 
-        console.log(userName, password)
-    }
-
-    return(
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type='text' onChange={changeUserName} value={userName} placeholder='User Name'/>
-                <input type='password' onChange={changepassword} value={password} placeholder='Password'/>
-                <button type='submit'>Login</button>
-            </form>
+  return (
+    <div style={{ height: "400px", width: "200px"}}>
+      <form
+        style={{ display: "flex", flexDirection: "column" , textAlign: 'center'}}
+      >
+        <input
+          type="text"
+          onChange={changeUserName}
+          value={userName}
+          placeholder="User Name"
+        />
+        <input
+          type="password"
+          onChange={changepassword}
+          value={password}
+          placeholder="Password"
+        />
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <button style={{ width: "100px" }} type="submit" onClick={handleLoginSubmit}>
+            Login
+          </button>
+          <button style={{ width: "100px" }} onClick={handleSignupSubmit}>Signup</button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
