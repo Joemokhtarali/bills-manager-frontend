ebcbc43d46238660bbfe4d459bf68db201bf5852
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-12-15 19:37:25 -0500

    setting up flexbox for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Login from '../forms/login'
+import React from "react";
+import Login from "../forms/login";
+import '../stylesheets/home.css'
 
-export default function Home(){
+export default function Home() {
+  return (
+    <div className='home'>
+      <div className='left'>
 
-    return(
-        <div>
-            <div> <Login /> </div>
-        </div>
+      </div>
+      <div className="right">
         
-    )
+        <h3>Welcome to Bills Manager</h3>
+        <p>
+          Bills Manager helps you keep track of all your Bills and Income for
+          the month, so you can keep track and control your spending.
+        </p>
+        <Login />
+      </div>
+    </div>
+  );
 }
